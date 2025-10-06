4f7d8b08ddd6698136bfca0285522227c46948e6
unknown <sam_skinner@yahoo.com>
2025-10-06 10:43:42 -0700

    Homework for week 1

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ console.log("Hello from JavaScript!");
 const button = document.querySelector("#myButton");
 button.addEventListener("click", () => {
   alert("You clicked the button!");
+  document.getElementById("myPara").style.color = "blue";
+  document.getElementById("myHeading").innerHTML = "I can change!";
 });
